7c0c27dbae7b7febeb75a1d0db8a2759ccb8faff
unknown <max.ibrahim43@hotmail.com>
2019-09-19 01:34:01 -0700

    designed interface of the home page and working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    image = models.ImageField(upload_to="images/",default = None)
+    #summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pubDate = models.DateTimeField()
+
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
+def home(request):
+    today = date.today()
+    year = today.year
+
+    return render(request,'jobs/home.html',{'year':year})
