5560f0eb6cdba82d8c0b5d7f716c4e013bd71634
unknown <max.ibrahim43@hotmail.com>
2019-09-20 00:51:57 -0700

    finished the jobs home page successfully

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from datetime import date
 
+from .models import Job
 # Create your views here.
 def home(request):
     today = date.today()
     year = today.year
 
-    return render(request,'jobs/home.html',{'year':year})
+    jobs = Job.objects
+    return render(request,'jobs/home.html',{'year':year,'jobs':jobs})
